079822b1423cd52bab4b56f672fb4e229d5a2141
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-04-21 14:40:57 +0530

    added some files

--- Middlewares/middleware02.js
+++ Middlewares/middleware02.js
@@ -1,1 +1,34 @@
 // create a middleware function that logs each incoming request's http method ,url ,and timestamp to the console
+const express = require("express")
+
+const app = express();
+
+function logged(req,res,next){
+    console.log("Method is " + req.method);
+    console.log("url is " + req.url);//this is the route we get 
+    console.log("route is " + req.hostname); //this will give you the host
+    console.log(new Date());
+    next();
+}
+
+// assignment - : 
+// 1 create a backend server in nodejs, that returns the sum endpoint
+// 2 create an html file , that hits the backend server using the fetch 'api'
+
+
+
+app.use(express.json());
+app.post('/mul',function(req,res){
+    const a = parseInt(req.body.a); 
+    const b=  parseInt(req.body.b);
+    res.json({
+        answer : a*b
+    })
+})
+
+
+
+
+
+
+// create a middleware that counts total number of reqeust sent to a server. aslo create a endpoint that exposees it
---
+++ Middlewares/middleware03.js
@@ -0,0 +1,32 @@
+// some commonly used middlewares or  predefiend middleware which we can reuire from outside instead of writing them
+
+// 1 express.json ->it is used to parse incoming request bodies that are formatted as json //this middleware is essential for handling josn payloads sent by clients in post or put requests
+
+const express = require("express");
+const cors = require("cors");
+const app = express()
+
+
+
+////  this error comes -:  it directly shows error Cannot read properties of undefined (reading 'a') 
+// the sol for it is in express , if you want to send hson data // you need to first parse the json data naswer given bellow
+app.use(express.json());
+app.use(cors())
+app.post('/mul',function(req,res){
+    console.log(req.body) // ye nahi karnege ot dikhega hi nahi kuch 
+    const a = parseInt(req.body.a); 
+    const b=  parseInt(req.body.b);
+    res.json({
+        answer : a*b
+    })
+})
+
+// 2 cors miidleware cors - Cross origin resource sharing
+// cors iss a security feature implemented by web browser that controls how resourses on a web server can be requested from another domain.
+// its a crucial mechanism for managing cross-origin requests and ensuring secure interactions between diffrernt origins on the web
+
+// by default in nodesjs cors origin request are blocked
+// if you want to make cors request you can use cors middleware
+
+
+app.listen(3003)
