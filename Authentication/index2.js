361a22232550d46e3e4482c5e255e9c09c3da70e
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-05-02 19:35:20 +0530

    learn about  the jwt

---
+++ Authentication/index2.js
@@ -0,0 +1,106 @@
+// create an endpoint /me that returns the user their information 'only if they send their data
+// jwts are a compact and self-contained way to represent information between two parties . they are commonly used
+// for authentication and information exchanges in web applications 
+
+// jwts are stateless . jet contains all the information needed to atuhentication a request , so the server doesn't need
+// to  store session data . all the data is stored in the token itself
+
+// why do we need jwts // there is a problem with using stateful tokens
+// stateful means that we need to store thse toens ein a variable right now and eventually in a dta base 
+// the problem is that we need to sne d a req to the databaeas every time the ser wants to hit an auth endpoint
+// so jwt essentially  prevents 1 round trip from thebackedn server and db fo any user req
+// jwt is not encription but it is encoding
+
+// now we have to create same signin signup logic without tokens and the uses is jwts
+
+const express = require("express");
+const jwt = require("jsonwebtoken")
+const app = express();
+app.use(express.json());
+const JWT_SECRET = "shubhankar"
+
+const users =[]
+
+
+app.post("/signup",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username : username,
+        password : password
+    })
+
+    res.json({
+        msg : "you are signed in"
+    })
+    console.log(users)
+})
+
+app.post("/signin",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    let foundUser = null;
+
+    for(let i = 0 ; i < users.length; i++){
+        if(users[i].username==username && users[i].password==password){
+            foundUser = users[i];
+        }
+    }
+
+    if(foundUser){
+        const token  = jwt.sign({  /// this will generate the token
+            username : username
+        },JWT_SECRET);
+
+
+        //foundUser.token = token; 
+        res.json({
+            token : token
+        })
+    }
+    else{
+       res.status(403).send({
+           mes: " invalid username or password"
+       })
+    }
+    console.log(users)
+})
+
+
+app.get("/me", function (req, res) {
+    const token = req.headers.token;
+    
+    try {
+        const decodedInformation = jwt.verify(token, JWT_SECRET); // can throw error
+        const username = decodedInformation.username;
+
+        let foundUser = null;
+
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username == username) {
+                foundUser = users[i];
+            }
+        }
+
+        if (foundUser) {
+            res.json({
+                username: foundUser.username,
+                password: foundUser.password
+            });
+        } else {
+            res.json({
+                msg: "token invalid"
+            });
+        }
+    } catch (err) {
+        res.status(403).json({
+            msg: "token invalid"
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
--- Authentication/jwt.js
+++
@@ -1,6 +0,0 @@
-// create an endpoint /me that returns the user their information 'only if they send their data
-// jwts are a compact and self-contained way to represent information between two parties . they are commonly used
-// for authentication and information exchanges in web applications 
-
-// jwts are stateless . jet contains all the information needed to atuhentication a request , so the server doesn't need
-// to  store session data . all the data is stored in the token itself
