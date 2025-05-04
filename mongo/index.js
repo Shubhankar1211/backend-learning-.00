2fe2d5a1f932fe498697f8887863d6663a393896
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-05-04 20:32:55 +0530

    mongo addition

---
+++ mongo/index.js
@@ -0,0 +1,7 @@
+// things we have to improve after assignement
+// password is not hashed 
+// a single crash(dublicate email) crashes the whole app
+// add more endpoint ( mark todo as done)
+// add timestamp at which todo was created/the time it needs to be done bhy
+// realtionships in mongo
+// add validatons to ensure emails and password are corect format 
