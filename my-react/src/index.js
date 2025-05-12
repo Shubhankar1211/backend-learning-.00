29b53c4ce055fda314e153d201627ba99ba479f0
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-05-12 12:41:06 +0530

    learned about the react

---
+++ my-react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'; // ye sare refrences lene ke kaam aati h
+import ReactDOM from 'react-dom/client';//
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
---
+++ reactvite/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Chai garam hai</h1>
+    )
+}
+
+export default Chai;
