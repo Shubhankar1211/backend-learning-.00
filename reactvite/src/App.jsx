ac2950bf18cf048080d9e87969dbc410cefe6d81
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-06-23 22:08:07 +0530

    simple changes

--- reactvite/src/App.jsx
+++ reactvite/src/App.jsx
@@ -137,20 +137,37 @@ console.log("counter");// this is how re-rendering like everytime counter run it
 // making the linkdin post card 
 function App() {
   return (
-
     <div style={{ background: "#dfe6e9", height: "100vh" }}>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div>
           <div>
-            <PostComponent />
+            <PostComponent
+              name="100xdevs"
+              followerCount="23,888"
+              time="12m"
+              image="https://thumbs.dreamstime.com/b/apple-logo-white-color-brand-front-store-las-vegas-120446030.jpg"
+              description="Learning React is fun!"
+            />
             <br />
           </div>
           <div>
-            <PostComponent />
+            <PostComponent
+              name="ReactJS"
+              followerCount="500k"
+              time="1h"
+              image="https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png"
+              description="Hooks, Context, and more!"
+            />
             <br />
           </div>
           <div>
-            <PostComponent />
+            <PostComponent
+              name="JavaScript Mastery"
+              followerCount="1M"
+              time="5h"
+              image="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+              description="Master modern JS."
+            />
           </div>
         </div>
       </div>
@@ -159,43 +176,55 @@ function App() {
 }
 
 const style = {
-  width: 200,
+  width: 250,
   backgroundColor: "white",
   borderRadius: 10,
   borderColor: "gray",
   borderWidth: 1,
   borderStyle: "solid", // important
   display: "flex", // fixed space
-  alignItems: "center", // optional: helps image and text align
-  padding: 8, // optional: adds some padding
-  gap: 10, // optional: adds spacing between image and text
+  flexDirection: "column",
+  padding: 8, // adds padding
+  gap: 8, // spacing between image and text
 };
 
-function PostComponent() {
-  return <div style={style}>
-    <div style={{ display: "flex" }}>
-      <img
-        src={
-          "https://thumbs.dreamstime.com/b/apple-logo-white-color-brand-front-store-las-vegas-120446030.jpg"
-        }
-        style={{
-          width: 20,
-          height: 20,
-          borderRadius: 20,
-        }} />
-      <div style={{ fontSize: 10, marginLeft: 10 }}>
-        <b>100xdevs</b>
-        <div>23,888 followers</div>
-        <div>12m</div>
+function PostComponent({ name, followerCount, time, image, description }) {
+  return (
+    <div style={style}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={
+            image ||
+            "https://thumbs.dreamstime.com/b/apple-logo-white-color-brand-front-store-las-vegas-120446030.jpg"
+          }
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 20,
+          }}
+          alt="profile"
+        />
+
+        <div style={{ fontSize: 10, marginLeft: 10 }}>
+          <b>{name}</b>
+          <div>{followerCount} followers</div>
+          <div style={{ display: "flex", alignItems: "center", gap: 4 }}>
+            <div>{time}</div>
+            <img src={""} style={{ width: 10, height: 10 }} alt="" />
+            {/* you can put some icon here */}
+          </div>
+        </div>
+      </div>
+
+      <div style={{ fontSize: 12 }}>
+        {description}
       </div>
     </div>
-    <div style={{ fontSize: 12 }}>
-      What to know how to win big? Check out how these folks won $6000 in bounties
-    </div>
-  </div>
+  );
 }
 
 export default App;
+
 
 // conditional rendering  kabhi kabhi i want to render this counter varibale render sometimes
 // react dom is the render of the react website
